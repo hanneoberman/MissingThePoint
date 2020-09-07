645282ae686a4cbd3bd8578260b7c859f865effc
Hanne <h.i.oberman@students.uu.nl>
2020-09-07 17:27:55 +0200

    Complete a single simulation run, without chain means and chain variances

--- R/impute.R
+++ R/impute.R
@@ -1,38 +1,88 @@
 # Impute the data
-impute <- function(amp, m_mech, p_inc, it_nr, imp_nr = 5){
-mids <- mice(amp[[m_mech]][[p_inc]], 
-          m = imp_nr,
-          method = "norm",
-          maxit = it_nr, 
-          printFlag = FALSE)
+impute <-
+  function(amp,
+           mids,
+           m_mech,
+           p_inc,
+           it_nr,
+           it_total = 50,
+           n_imp = 5) {
+    
+    # initialize the mids object in the first iteration
+    if (it_nr == 1) {
+      mids <<- mice(
+        amp[[m_mech]][[p_inc]],
+        m = n_imp,
+        method = "norm",
+        maxit = 1,
+        printFlag = FALSE
+      )
+    }
+    
+    # append to the mids object in any further iteration
+    if (it_nr > 1) {
+      mids <<- mice.mids(mids, printFlag = FALSE)
+    }
+    
+    # analyze and pool imputations
+    mipo <- mids %>%
+      mice::lm.mids(Y ~ ., .) %>%
+      mice::pool() %>%
+      .$pooled
+    
+    # extract statistics
+    est <- mipo$estimate[2]
+    SE <- sqrt(mipo$b + (mipo$b / n_imp)) %>% .[2]
+    ci_lo <- est - qt(.975, df = n_imp - 1) * SE
+    ci_up <- est + qt(.975, df = n_imp - 1) * SE
+    
+    # add convergence parameters
+    mild <- mids %>% mice::complete("all")
+    qhat <-
+      map_dbl(mild, ~ {
+        lm(formula = Y ~ ., data = .) %>% .$coefficients %>% .[2]
+      })
+    lambda <- mild %>%
+      purrr::map_dbl(., ~ {
+        princomp(., cor = TRUE) %>% .$sdev %>% .[1] %>% . ^ 2 #first eigenvalue of the varcovar matrix
+      })
+    
+    # save for each simulation condition
+    out <- data.frame(
+      mech = m_mech,
+      p = p_inc,
+      it = it_nr,
+      est = est,
+      CIW = ci_up - ci_lo,
+      cov = ci_lo < Q & Q < ci_up,
+      qhat = t(qhat),
+      lambda = t(lambda)
+    )
+    
+    # save in global environment
+    # if (it_nr == it_total) {
+    #   chain_means <<-
+    #     data.frame(
+    #       mech = rep(m_mech, it_total),
+    #       p = rep(p_inc, it_total),
+    #       it = 1:it_total,
+    #       chain.mean.Y = mids$chainMean["Y", ,],
+    #       chain.mean.X1 = mids$chainMean["X1", ,],
+    #       chain.mean.X2 = mids$chainMean["X2", ,]
+    #     )
+    #   
+    #   chain_vars <<-
+    #     data.frame(
+    #       mech = m_mech,
+    #       p = p_inc,
+    #       it = 1:it_total,
+    #       chain.var.Y = mids$chainVar["Y", ,],
+    #       chain.var.X1 = mids$chainVar["X1", ,],
+    #       chain.var.X2 = mids$chainVar["X2", ,]
+    #     )
+    # }
+    return(out)
+  }
 
-mipo <- mids %>% 
-  mice::lm.mids(Y ~ ., .) %>% 
-  mice::pool() %>% 
-  .$pooled
-
-est <- mipo$estimate[2]
-SE <- sqrt(mipo$b + (mipo$b / mipo$m)) %>% .[2]
-ci_lo <- est - qt(.975, df = imp_nr - 1) * SE 
-ci_up <- est + qt(.975, df = imp_nr - 1) * SE 
-
-pca <- mids %>% mice::complete("all") %>% 
-  purrr::map_dbl(., ~ {
-    princomp(., cor = TRUE) %>% .$sdev %>% .[1] %>% . ^ 2 #first eigenvalue of the varcovar matrix
-  })
-
-out <- data.frame(
-  mm = m_mech,
-  p = p_inc,
-  it = it_nr,
-  est = est,
-  CIW = ci_up - ci_lo,
-  cov = ci_lo < Q & Q < ci_up,
-  pca = t(pca)
-)
-
-return(out)
-}
-
-# test
-impute(amp = amp, m_mech = "MCAR", p_inc = 0.5, it_nr = 1, imp_nr = 5)
+# # test
+# impute(amp = amp, mids = mids, m_mech = "MCAR", p_inc = 0.5, it_nr = 1, n_imp = 5)
--- RunSimulation.R
+++ RunSimulation.R
@@ -7,11 +7,22 @@ setup(seed = 1111)
 # ampute the data
 amp <- map(m_mech, function(mm) {
   map(p_inc, function(pp) {
-  mice::ampute(
-    data = dat,
-    prop = pp,
-    patterns = amp_pat,
-    mech = "MCAR"
-  )$amp
-})%>% set_names(as.character(p_inc))}) %>% set_names(as.character(m_mech))
+    mice::ampute(
+      data = dat,
+      prop = pp,
+      patterns = amp_pat,
+      mech = "MCAR"
+    )$amp
+  }) %>% set_names(as.character(p_inc))
+}) %>% set_names(as.character(m_mech))
 
+results <- map_dfr(m_mech, function(mm) {
+  map_dfr(p_inc, function(pp) {
+    map_dfr(1:50, function(it) {
+  impute(amp,
+  mids,
+  m_mech = mm,
+  p_inc = pp,
+  it_nr = it)})
+  })
+})
