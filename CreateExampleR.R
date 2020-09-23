ba3e5e0a6e3f816492b1bfea8b2a18b81f3aa1b1
Your Name <you@example.com>
2020-09-23 10:00:41 +0000

    Extend example with plots

---
+++ CreateExampleR.R
@@ -0,0 +1,16 @@
+# Plot example of convergence diagnostics and outcomes
+
+# setup
+source("~/MissingThePoint/R/setup_example.R")
+setup_example(seed=11)
+ests <- purrr::map_dfr(1:n_it, function(it){impute_once(iteration = it)})
+param <- preprocess_thetas(mids$chainMean, ext = "mu") %>% 
+  dplyr::full_join(
+    preprocess_thetas(mids$chainVar, ext = "sigma") %>% 
+      dplyr::full_join(qhats) %>% 
+      dplyr::full_join(lambdas)
+  )
+convs <- get_diagnostics(param)
+example <- full_join(ests, convs)
+save(example, file = "Data/example.Rdata")
+save(mids, file = "Data/example_mids.Rdata")
---
+++ R/setup_example.R
@@ -0,0 +1,80 @@
+# setup environment for example
+
+setup_example <- function(seed){
+  # environment
+  library(mice)
+  library(tidyverse)
+  set.seed(seed)
+  
+  # parameters
+  n_imp <<- 5
+  n_it <<- 25
+  mids <<- qhats <<- lambdas <<- NULL  
+}
+
+# other functions to load
+source("~/MissingThePoint/R/ComputeDiagnostics.R")
+
+not_all_na <- function(x) {
+  !all(is.na(x))
+}
+
+preprocess_thetas <- function(theta, ext){
+  out <- theta %>% 
+    as.data.frame() %>% 
+    t() %>% 
+    as.data.frame() %>%
+    setNames(paste0(ext, ".", names(.))) %>%
+    cbind(
+      it = 1:n_it,
+      m = rep(1:n_imp, each = n_it),
+      .
+    ) %>% 
+    select_if(not_all_na) %>% 
+    arrange(it)
+  rownames(out) <- NULL
+  return(out)
+}
+
+get_diagnostics <- function(parameters) {
+  thetas <- names(parameters)[-c(1:2)]
+  out <- purrr::map_dfc(thetas, function(tt) {
+    matrix(parameters[[tt]], ncol = n_imp, byrow = T) %>%
+      convergence() %>%
+      select(r.hat.max, ac.max) %>%
+      setNames(., paste0(names(.), ".", tt))
+  }) %>%
+    cbind(it = 1:n_it, .)
+  return(out)
+}
+
+impute_once <- function(iteration, ...) {
+  if(is.null(mids)){
+    mids <<- mice(boys, m = n_imp, maxit = 1, printFlag = FALSE)
+  }
+  else {
+    mids <<- mice.mids(mids, printFlag = FALSE)
+  }
+  mira <- mids %>% mice::lm.mids(age ~ ., data = .) 
+  mipo <- mira %>%
+    mice::pool() %>%
+    summary(., conf.int = TRUE)
+  mild <- mids %>% mice::complete("all")
+  
+  qhat <- 
+    map_dbl(mild, ~ {
+      lm(formula = age ~ ., data = .) %>% .$coefficients %>% .[2]
+    })
+  lambda <- mild %>%
+    purrr::map_dbl(., ~ {
+      dplyr::mutate(., dplyr::across(dplyr::everything(), ~as.numeric(.))) %>% 
+        princomp(., cor = TRUE) %>% .$sdev %>% .[1] %>% . ^ 2 #first eigenvalue of the varcovar matrix
+    })
+  qhats <<- rbind(qhats, data.frame(it = iteration, m = 1:n_imp, qhat = qhat))
+  lambdas <<- rbind(lambdas, data.frame(it = iteration, m = 1:n_imp, lambda = lambda))
+  
+  out <- data.frame(it = iteration, est = mipo[["estimate"]][2])
+  return(out)
+}
+
+
