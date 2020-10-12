f743e7f295aeb63dad970cec9ee7ca28a9ebd854
Oberman <h.i.oberman@uu.nl>
2020-10-12 17:16:18 +0200

    Add automatically updated references

--- R/ComputeDiagnostics.R
+++ R/ComputeDiagnostics.R
@@ -9,8 +9,8 @@ source('R/ComputeAC.R')
 convergence <- function(x, include_acf = FALSE) {
   # input: object with imputation chain(s), e.g. chain mean, choice of calculating default AC
   # output: AC values at each iteration, computed manually, if necessary also using stats:acf() 
-  out <-  left_join(rhat_adapted(x), ac_adapted(x), by = "iteration") 
-  if(include_acf){ out <- left_join(out, ac_adapted(x, "acf"), by = "iteration")
+  out <-  full_join(rhat_adapted(x), ac_adapted(x), by = "iteration") 
+  if(include_acf){out <- full_join(out, ac_adapted(x, "acf"), by = "iteration")
   }
   out <- out %>% dplyr::rename(it = iteration)
   return(out)
